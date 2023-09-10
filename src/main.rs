b317f2e3aa5ef53a0846610930b2b5967f03c92a
bucky0112 <bucky0112@gmail.com>
2023-09-11 00:32:21 +0800

    Add support for generating QR codes in SVG format with customizable foreground and background colors

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,19 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer};
 use image::{DynamicImage, Luma};
+use qrcode::render::svg;
 use qrcode::QrCode;
-// use qrcode::render::svg;
+use regex::Regex;
 
-// #[get("/")]
-// async fn index() -> HttpResponse {
-//     let code = QrCode::new(b"https://buckychu.im").unwrap();
-//     let image = code.render()
-//         .min_dimensions(200, 200)
-//         .dark_color(svg::Color("#800000"))
-//         .light_color(svg::Color("#ffff80"))
-//         .build();
+fn is_valid_color(color: &str) -> bool {
+    let re = Regex::new(r"^#[0-9a-fA-F]{6}$").unwrap();
+    re.is_match(color)
+}
 
-//     HttpResponse::Ok()
-//         .content_type("image/svg+xml")
-//         .body(image)
-// }
 #[derive(serde::Deserialize)]
 struct Info {
     url: String,
+    foreground: Option<String>,
+    background: Option<String>,
 }
 
 #[get("/generate_qr")]
@@ -39,9 +34,36 @@ async fn index(data: web::Query<Info>) -> HttpResponse {
     HttpResponse::Ok().content_type("image/png").body(buffer)
 }
 
+#[post("/generate_qr_svg")]
+async fn generate_svg(data: web::Json<Info>) -> HttpResponse {
+    let fg_color_str = match &data.foreground {
+        Some(color) if is_valid_color(color) => color,
+        _ => "#000000",
+    };
+
+    let bg_color_str = match &data.background {
+        Some(color) if is_valid_color(color) => color,
+        _ => "#FFFFFF",
+    };
+
+    let code = match QrCode::new(data.url.as_bytes()) {
+        Ok(c) => c,
+        Err(_) => return HttpResponse::BadRequest().body("你輸入的字串無法處理"),
+    };
+
+    let image = code
+        .render()
+        .min_dimensions(200, 200)
+        .dark_color(svg::Color(fg_color_str))
+        .light_color(svg::Color(bg_color_str))
+        .build();
+
+    HttpResponse::Ok().content_type("image/svg+xml").body(image)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new().service(index).service(generate_svg))
         .bind("127.0.0.1:8080")?
         .run()
         .await
