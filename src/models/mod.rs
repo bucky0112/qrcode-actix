4ce290b5266f867961a63c466856a5257276513e
bucky0112 <bucky0112@gmail.com>
2023-09-20 09:45:21 +0800

    Add email and address fields to the Info struct

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,7 +1,9 @@
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, serde::Serialize)]
 pub struct Info {
     pub url: Option<String>,
     pub phone: Option<String>,
+    pub email: Option<String>,
+    pub address: Option<String>,
     pub foreground: Option<String>,
     pub background: Option<String>,
     pub dimension: Option<u32>,
