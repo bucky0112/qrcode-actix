308d04e4675d966094d38a762a4c5007242ab8ba
bucky0112 <bucky0112@gmail.com>
2023-10-09 14:14:24 +0800

    Add CORS support to the Actix web server in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{App, HttpServer};
+use actix_web::{http, App, HttpServer};
+use actix_cors::Cors;
 
 mod models;
 mod api;
@@ -6,7 +7,16 @@ use api::{generate_svg, index};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index).service(generate_svg))
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            // .allowed_origin("http://localhost:3000")
+            .allowed_origin_fn(|_, _| true)
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
+        App::new().wrap(cors).service(index).service(generate_svg)
+    })
         // .bind("127.0.0.1:8080")?
         // .bind("localhost:3000")?
         .bind("0.0.0.0:8080")?
